b8d510e1d4f56b500b5a11ec1ee34c44ba0c4aa6
Shrutii90 <shrutiimaharana@gmail.com>
2025-03-31 19:42:42 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Debates from './pages/Debates';
 import Leaderboard from './pages/Leaderboard';
 import DebateRoom from './pages/DebateRoom';
+import socket from './pages/socket';
+
 
 function App() {
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("Connected to server with ID:", socket.id);
+    });
+    socket.on("message", (data) => {
+      console.log("Received message:", data);
+    });
+
+    return () => {
+      socket.disconnect();
+      console.log("Disconnected from server with ID:", socket.id);
+    };
+  }, []);
   return (
     <Router>
       <div className="min-h-screen bg-[#0a0b1e] text-gray-100">
